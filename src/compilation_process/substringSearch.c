9ccac58b0fe4e18ec0a2881d3a50f74840b6b5a0
Kirill Guryanov <kg580652@gmail.com>
2025-09-30 23:46:18 +0300

    Task: Finding a substring

---
+++ src/compilation_process/substringSearch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int substringSearch(const char *s, const char *s1) {
+  int count = 0;
+  const char *pos = s;
+  while ((pos = strstr(pos, s1)) != NULL) {
+    count++;
+    pos += strlen(s1);
+  }
+  return count;
+}
+
+int main() {
+  char s[] = "ababa";
+  char s1[] = "ba";
+  int result = substringSearch(s, s1);
+  printf("Количество вхождений '%s' в '%s': %d\n", s1, s, result);
+  return 0;
+}
