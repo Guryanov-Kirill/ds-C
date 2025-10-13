d683dc6a2e6523cea410f5bd98b2b9378c98ed23
Kirill Guryanov <kg580652@gmail.com>
2025-10-13 14:57:15 +0300

    fibonacci

---
+++ src/Fib.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 0;
+    int b = 1;
+    int count = 0;
+    int t;
+    for (int i = 0; i < 1000000; i++) {
+        t = a + b;
+        a = b;
+        b = t;
+        if (b <= 1000000){
+            if (b % 2 == 0){
+                count = count + b;
+                printf("%d\n", b);
+            }
+        } else {
+            break;
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
