34d78d190e68450ae3ef9a21a27908fdf3d8f3d4
Kirill Guryanov <kg580652@gmail.com>
2025-12-21 23:04:42 +0300

    bugs fixed

--- src/stack.c
+++ src/stack.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Stack {
+    struct StackNode* head;
+};
+
 struct Stack new(void)
 {
     struct Stack stack = {
@@ -10,24 +14,28 @@ struct Stack new(void)
     return stack;
 }
 
-void push(struct Stack* stack, int value)
+bool push(struct Stack* stack, int value)
 {
     struct StackNode* node = malloc(sizeof(struct StackNode));
+    if (node == NULL) {
+        return false;
+    }
     node->value = value;
     node->next = stack->head;
     stack->head = node;
+    return true
 }
 
 int pop(struct Stack* stack)
 {
     if (stack->head != NULL) {
         struct StackNode* oldNode = stack->head;
-        int res = oldNode->value;
+        int result = oldNode->value;
         stack->head = oldNode->next;
         free(oldNode);
-        return res;
+        return result;
     } else {
-        return -1;
+        return errorCode;
     }
 }
 
@@ -36,7 +44,7 @@ int peek(struct Stack* stack)
     if (stack->head != NULL) {
         return stack->head->value;
     } else {
-        return -1;
+        return errorCode;
     }
 }
 
